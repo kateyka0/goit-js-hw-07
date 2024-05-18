49095d325cd5cc174ac2f363715be4d996ecc5ed
Katrin <kateyka1410@gmail.com>
2024-05-18 19:07:11 +0300

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,50 @@
-function getShippingMessage(country, price, deliveryFee) {
-    const totalPrice = price + deliveryFee;
-    const message = `Shipping to ${country} will cost ${totalPrice} credits`;
-    return message;
-}
-console.log(getShippingMessage("Australia", 120, 50)); 
-console.log(getShippingMessage("Germany", 80, 20));
-console.log(getShippingMessage("Sweden", 100, 20));
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    alt: "Alpine Spring Meadows",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    alt: "Nature Landscape",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    alt: "Lighthouse Coast Sea",
+  }
+];
+const gallery = document.querySelector(".gallery");
+const galleryItems = images.map(({ url, alt }) => `<li class="gallery-item">
+    <img src="${url}" alt="${alt}" class="gallery-image">
+  </li>`).join('');
+gallery.insertAdjacentHTML("beforeend", galleryItems);
+const style = document.createElement('style');
+style.textContent = `
+  .gallery {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 24px;
+    list-style: none;
+    padding: 0;
+  }
+  .gallery-item {
+    flex:  calc(33.333% - 24px);
+    box-sizing: border-box;
+  }
+  .gallery-image {
+    width: 100%;
+    height: auto;
+    display: block;
+  }`;
+document.head.append(style);
